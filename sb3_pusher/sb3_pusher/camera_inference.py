8a1e2459aba556c1720951ac05b9bddc0ad11d50
Yannik Braendle <y.braendle@th-bingen.de>
2025-02-13 12:34:28 +0100

    lerped velocity added to camera inference

--- sb3_pusher/sb3_pusher/camera_inference.py
+++ sb3_pusher/sb3_pusher/camera_inference.py
@@ -1,21 +1,29 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32, Bool
+from std_msgs.msg import Float32, Bool, Empty
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PointStamped, Point
 from geometry_msgs.msg import Twist
+from visualization_msgs.msg import Marker
 from tf2_ros import Buffer, TransformListener
 from tf2_geometry_msgs import do_transform_point
 import numpy as np
 import tf2_ros
 from stable_baselines3 import PPO
 import math
+import time
+import argparse
 
 class StableBaselines3Inference(Node):
-    def __init__(self):
+    FACTOR = 2.4
+    TWIST_PUB_FREQ = 0.1
+    TWIST_ANG_RATE = 2.5 * TWIST_PUB_FREQ
+    TWIST_VEL_RATE = 0.5 * TWIST_PUB_FREQ
+    def __init__(self, suppress_driving:bool = False):
         super().__init__('stable_baselines3_inference')
         # Set up QoS profile for LiDAR
-        
+        self.get_logger().info("Setting up...")
+
         self.qos_profile = rclpy.qos.QoSProfile(
             reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
             history=rclpy.qos.HistoryPolicy.KEEP_LAST,
@@ -36,7 +44,23 @@ class StableBaselines3Inference(Node):
             self.ball_angle_callback,
             10,
         )
-        
+
+        self.clicked_point_subscription = self.create_subscription(
+            PointStamped,
+            "/clicked_point",
+            self.clicked_point_callback,
+            self.qos_profile
+        )
+
+        self.reset_target_subscription = self.create_subscription(
+            Empty,
+            "/reset_target",
+            self.reset_target_callback,
+            1
+        )
+        self.target_point = None
+        self.start_time = 0.0
+        self.target_pub = self.create_publisher(Marker, "/target_pos", 1)
         # Subscription to the SonarSensorInfo topic
         self.sonar_subscription = self.create_subscription(
             Float32,
@@ -57,14 +81,23 @@ class StableBaselines3Inference(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self.model = PPO.load("/home/turtle1/turtlebot3_ws/cardinal_locator_1999920_steps.zip")
+        self.model = PPO.load("/home/turtle1/turtlebot3_ws/pusher_lerp_2499600_steps.zip")
 
         self.twist_pub = self.create_publisher(Twist, '/cmd_vel', 1)
-
+        self.twist_vel = 0.0
+        self.vel_target = 0.0
+        self.twist_ang = 0.0
+        self.ang_target = 0.0
         # timer for inference tick
-        self.create_timer(0.1, self.inference_tick)
+        self.create_timer(0.26, self.inference_tick)
+        self.create_timer(self.TWIST_PUB_FREQ, self.twist_pub_loop)
         
         self.ball_dir_vec = (0,0)
+        self.get_logger().info("Setup finished...")
+        self.suppress_driving: bool = suppress_driving
+        if suppress_driving:
+            self.get_logger().warn("Driving supressed! Drop the -sd or --suppress_driving flag to resume driving!")
+        self.reset_target_marker()
 
     def sonar_distance_callback(self, msg):
         self.sonar_distance = msg.data
@@ -107,7 +140,8 @@ class StableBaselines3Inference(Node):
                 # If no valid neighbour was found, set the value to 0.
                 if not found_valid:
                     value = 0.0
-
+            # Adjust value to be like the godot values
+            value = (4.215 - value) / 4.215
             # Append the (possibly corrected) value to our result list.
             lidar_scan.append(value)
 
@@ -150,11 +184,78 @@ class StableBaselines3Inference(Node):
                 dir_stamped.point.x = self.sonar_distance + 0.09
                 self.ball_pos = do_transform_point(dir_stamped, map_tf)
                 self.ball_pos_pub.publish(self.ball_pos)
-                
-                        
-        # self.get_logger().info(f"Ball Angle/Visible: ({self.ball_angle}|{self.ball_visible})")
+
+    def clicked_point_callback(self, msg):
+        self.target_point = msg
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.id = 0
+        marker.type = Marker.CYLINDER
+        marker.pose.position.x = msg.point.x
+        marker.pose.position.y = msg.point.y
+        marker.scale.x = 0.3
+        marker.scale.y = 0.3
+        marker.scale.z = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 1.0
+        marker.color.a = 0.5
+        self.target_pub.publish(marker)
+        self.get_logger().info(f"New target set at: ({msg.point})")
+        self.start_time = time.time()
+
+    def reset_target_marker(self):
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.id = 0
+        marker.type = Marker.DELETE
+        self.target_pub.publish(marker)
+
+    def calc_vec_dist(self, point):
+        return math.sqrt(point.x * point.x + point.y * point.y)
+
+    def pub_stop_twist(self):
+        self.vel_target = 0.0
+        self.ang_target = 0.0
+        self.twist_vel = 0.0
+        self.twist_ang = 0.0
+
+    def move_toward(self, src, tgt, step):
+        if src < tgt:
+            if step >= tgt - src:
+                return tgt
+            else:
+                return src + step
+        else:
+            if step >= src - tgt:
+                return tgt
+            else:
+                return src - step
+            
+    def twist_pub_loop(self):
+        # move toward on vel and ang
+        self.twist_vel = self.move_toward(self.twist_vel, self.vel_target, self.TWIST_VEL_RATE)
+        self.twist_ang = self.move_toward(self.twist_ang, self.ang_target, self.TWIST_ANG_RATE)
+        twist = Twist()
+        twist.linear.x = self.twist_vel
+        twist.angular.z = self.twist_ang
+        if not self.suppress_driving:
+            self.twist_pub.publish(twist)
+
+    def reset_target_callback(self, msg):
+        self.get_logger().warn("Reset target signal receieved! Resetting...")
+        self.target_point = None
+        self.reset_target_marker()
 
     def inference_tick(self):
+        if self.target_point == None:
+            self.pub_stop_twist()
+            return
+        try:
+            tf_to_local = self.tf_buffer.lookup_transform("base_link", "map", rclpy.time.Time())
+        except:
+            self.get_logger().error("Transform error...")
+            return
         # if self.ball_pos is None:
         #     self.get_logger().warning("No ball position received yet")
         try:
@@ -162,14 +263,15 @@ class StableBaselines3Inference(Node):
         except:
             self.get_logger().error("Transform error...")
             return
-        point_target = PointStamped()
-        point_target.header.frame_id = "base_link"
-        point_target.header.stamp = self.get_clock().now().to_msg()
-        point_target.point = Point()
-        point_target.point.x = 0.0
-        point_target.point.y = 0.0
-        point_target = do_transform_point(point_target, tb_pos)
-        ball_pos_rel = do_transform_point(self.ball_pos, tb_pos)
+        point_target = self.target_point
+        # point_target = PointStamped()
+        # point_target.header.frame_id = "base_link"
+        # point_target.header.stamp = self.get_clock().now().to_msg()
+        # point_target.point = Point()
+        # point_target.point.x = 0.0
+        # point_target.point.y = 0.0
+        point_target = do_transform_point(point_target, tf_to_local)
+        ball_pos_rel = do_transform_point(self.ball_pos, tf_to_local)
 
  #^       self.get_logger().info(f"""ball vec: {str((
  #^           self.ball_dir_vec[0],
@@ -193,13 +295,16 @@ class StableBaselines3Inference(Node):
         obs["obs"].extend(self.lidar_scan)
         # self.get_logger().info(f"Observation: ({obs['obs'][2]}|{obs['obs'][3]})")
         action, _states = self.model.predict(obs)
-        self.get_logger().info(f"Action: {action}")
-        twist = Twist()
-
-        twist.linear.x = - action[0] * 0.20
-        twist.angular.z = action[1] * 2.0
-
-        self.twist_pub.publish(twist)
+        # swap action places
+        # tmp = action[0]
+        # action[0] = action[1]
+        # action[1] = tmp
+        obsv = obs["obs"]
+        self.get_logger().info(f"\nTarget: ({point_target.point.x:.4f}|{point_target.point.y:.4f}) \nObs: ({obsv[0]:.4f}|{obsv[1]:.4f}) \nAction: ({action[0]:.4f}|{action[1]:.4f})")
+        # self.get_logger().info(f"Action: {action}")
+        # TODO: implement twist lerp
+        self.vel_target = np.clip(action[0] * 0.20, -0.10, 0.20)
+        self.ang_target = action[1] * 2.0
         
         self.ball_dir_vec = (0,0)
         
@@ -208,7 +313,11 @@ class StableBaselines3Inference(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    inference = StableBaselines3Inference()
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sd', '--suppress_driving', action='store_true', help='Suppress driving functionality')
+    parsed_args, unknown = parser.parse_known_args()
+    inference = StableBaselines3Inference(suppress_driving=parsed_args.suppress_driving)
     rclpy.spin(inference)
     inference.destroy_node()
     rclpy.shutdown()
